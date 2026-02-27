7dc421c30504e64e1a29056020fd229e5b57c55d
diegonoram <diegonorams@gmail.com>
2026-02-26 23:21:30 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("8644780419:AAHMPFnGkmlPuuQ4NTNbUVEW7J3-o4iYRl8")
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 Hola. Soy NemoBot.")
